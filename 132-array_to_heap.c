592d389096db86611bced0156f84f329db4f48ed
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-09-23 15:04:06 -0700

    function that builds a Max Binary Heap tree from an array

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -1,39 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_rotate_left - left child of root becomes new root, tree rotated
- * so it retains BST ordering of values (in-order traversal of leaves is same)
- * @tree: tree to left rotate
- * Return: pointer to new root node, or NULL if `root` is NULL
+ * array_to_heap - Builds a Binary Heap from an array.
+ * @array: The pointer to the first element of the array.
+ * @size: The number of element in the array.
+ * Return: The pointer to the root node of the created Binary Heap.
  */
-binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+heap_t *array_to_heap(int *array, size_t size)
 {
-	binary_tree_t *pivot;
+	heap_t *root = NULL;
+	size_t i;
 
-	if (!tree)
-		return (NULL);
-
-	/* pivot will become new root */
-	pivot = tree->right;
-
-	/* migrate children to keep BST order */
-	tree->right = pivot->left;
-	if (pivot->left)
-		pivot->left->parent = tree;
-
-	/* handle upstream connections if `tree` is a subtree */
-	pivot->parent = tree->parent;
-	if (tree->parent)
-	{
-		if (tree == tree->parent->left)
-			tree->parent->left = pivot;
-		else
-			tree->parent->right = pivot;
-	}
-
-	/* finally rotate pivot into root postion */
-	pivot->left = tree;
-	tree->parent = pivot;
-
-	return (pivot);
+	for (i = 0; array && i < size; i++)
+		heap_insert(&root, array[i]);
+	return (root);
 }
